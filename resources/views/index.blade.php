36f0814b13635eaa493c67ec53706f641479dcaf
Ismael Semmar Galvez <xgalan@hotmail.com>
2022-11-02 20:41:49 +0100

    Afegir estil basic a la vista

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,7 +4,16 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>MP09 - Ismael Semmar</title>
+
+    <style>
+
+        body{
+            background-color: #f5f5f5;
+            text-align: center;
+        }
+
+    </style>
 </head>
 <body>
     <h1><?=$greeting;?></h1>
